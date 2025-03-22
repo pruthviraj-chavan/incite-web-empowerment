d4023f2d864848b54d08abc8fe7d33bb2a9efb14
Pruthviraj Chavan <91907487+pruthviraj-chavan@users.noreply.github.com>
2025-03-23 00:16:00 +0530

    Update HomePage.tsx

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -3,6 +3,7 @@ import { useEffect } from 'react';
 import { Hero, Services, CoursesOverview, InspirationSection, Testimonials, GalleryPreview, CTASection } from '../components/HomePage';
 import OurTeam from '../components/OurTeam';
 import AdPopup from '../components/AdPopup';
+import MarketingPostersSection from '../components/MarketingPostersSection';
 
 const HomePage = () => {
   useEffect(() => {
@@ -15,6 +16,7 @@ const HomePage = () => {
       <Hero />
       <Services />
       <CoursesOverview />
+      <MarketingPostersSection/>
       <InspirationSection />
       <OurTeam />
       <Testimonials />
