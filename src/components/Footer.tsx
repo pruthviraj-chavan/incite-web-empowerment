a30b91d5b1adcea097338fdf49543302df467cdc
Pruthviraj Chavan <91907487+pruthviraj-chavan@users.noreply.github.com>
2025-04-06 20:36:17 +0530

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -62,7 +62,7 @@ const Footer = () => {
         
         <div className="border-t border-gray-200 mt-8 pt-6">
           <p className="text-center text-gray-500 text-sm">
-            © {currentYear} Made By dieVektor.tech , Powered By Incite Computers, Radhanagari. All rights reserved.
+            © {currentYear} Incite Computers | Designed by dieVektor.tech | All rights reserved
           </p>
         </div>
       </div>
